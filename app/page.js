67a3d464e6a6478d6418978f807ca784287d1919
Jakaria030 <gulamjakaria99@gmail.com>
2025-01-24 10:45:21 +0600

    Fetching all post and showing in home page task is done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+import getAllPost from "@/lib/getAllPost";
+import Link from "next/link";
 
-const Home = () => {
+const Home = async () => {
+  const blogs = await getAllPost();
+
   return (
-    <div>
-      Hello
-    </div>
+    <main className="max-w-7xl mx-auto px-5 my-5 md:my-10">
+      {
+        blogs.map((blog, indx) => (
+          <Link href={`/blog/${blog.id}`} key={blog.id}>
+            <div className="mt-5 bg-slate-100 border-l-4 border-[#313131] py-3 flex items-center gap-3 hover:bg-slate-200 transition-colors duration-100 ease-in-out">
+              <div className="bg-[#313131] text-white px-2 py-1 font-medium shrink-0">Blog: {(indx + 1).toString().padStart(3, '0')}</div>
+              <p className="capitalize">{blog.title}</p>
+            </div>
+          </Link>
+        ))
+      }
+    </main>
   );
 };
 
